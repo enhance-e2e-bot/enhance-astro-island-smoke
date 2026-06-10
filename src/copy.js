ba4fddcdaf0120d23379f938686ffb6984d5f81f
Haziq Nordin <hbnordin2@gmail.com>
2026-06-11 00:00:27 +1000

    chore: prep for e2e reproducible clone

--- src/copy.js
+++ src/copy.js
@@ -1,11 +1,11 @@
 export const copy = {
-  heading: "astro island initial heading",
-  staticAction: "Astro static initial action",
-  generatedAction: "Astro generated initial action",
-  duplicateSecond: "Astro island duplicate second initial action",
-  duplicateThird: "Astro island duplicate third initial action",
-  islandPortal: "Astro island portal initial action",
-  account: "Astro account initial action",
-  slow: "Astro slow initial action",
-  oauth: "Astro OAuth initial action"
+  heading: "astro island HMR heading after smoke",
+  staticAction: "Astro static HMR action",
+  generatedAction: "Astro generated HMR action",
+  duplicateSecond: "Astro island duplicate second HMR action",
+  duplicateThird: "Astro island duplicate third HMR action",
+  islandPortal: "Astro island portal HMR action",
+  account: "Astro account HMR action",
+  slow: "Astro slow HMR action",
+  oauth: "Astro OAuth HMR action"
 };
